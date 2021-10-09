9be2f0b874c4ce5ee240d018dea12e9b45cb962f
pink-eye <github@virginson.anonaddy.com>
2021-10-09 13:55:44 +0630

    improve toasts a little bit

--- src/js/components/toast.js
+++ src/js/components/toast.js
@@ -7,21 +7,22 @@ const showToast = (type, text) => {
 	if (!isExistSimilarToast(text)) {
 		let toastList = _io_q('.toast__list')
 
-		toastList.insertAdjacentHTML('beforeEnd', createToastItemHTML(type, text))
+		toastList.insertAdjacentHTML('afterBegin', createToastItemHTML(type, text))
 
 		let toastItemAll = toastList.querySelectorAll('.toast__item');
-		let lastToastItem = toastItemAll[toastItemAll.length - 1];
+		let firstToastItem = toastItemAll[0];
 
-		setTimeout(_ => { lastToastItem.classList.add('_visible') }, 15);
+		setTimeout(_ => { firstToastItem.classList.add('_visible') }, 15);
 
 		setTimeout(_ => {
-			lastToastItem.classList.remove('_visible')
+			firstToastItem.classList.remove('_visible')
 
 			setTimeout(_ => {
-				lastToastItem.remove()
+				firstToastItem.remove()
 
 				toastItemAll = null
 				toastList = null
+				firstToastItem = null
 			}, 4000)
 
 		}, 5000)
