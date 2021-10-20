ea015aaf2e55a5034550d54c10c2e8c45094bd49
pink-eye <github@virginson.anonaddy.com>
2021-10-20 11:15:57 +0630

    change implementation & ui of suggestions a little bit

--- src/js/components/suggest.js
+++ src/js/components/suggest.js
@@ -1,70 +1,41 @@
 let lastSelected = null;
 
-const showSuggest = (parent, data) => {
-	let suggestAll = parent.querySelectorAll('.search__suggest');
+const createSuggestHTML = textContent => `<button class="search__suggest">${textContent}</button>`
 
-	if (!parent.classList.contains('_has-suggest'))
-		parent.classList.add('_has-suggest')
+const addSuggest = (parent, data) => {
+	let searchDropdown = parent.querySelector('.search__dropdown');
+	let searchBar = parent.querySelector('.search__bar');
 
-	if (suggestAll.length > 0) {
-		for (let index = 0, length = suggestAll.length; index < length; index++) {
-			let suggest = suggestAll[index];
+	if (!searchDropdown.firstChild) {
+		for (let index = 0, length = 10; index < length; index++) {
+			const query = searchBar.value.trim()
 
-			if (data[index]) {
-				suggest.textContent = data[index]
-				suggest.classList.add('_visible');
-			}
-
-			suggest = null
+			if (!isEmpty(data[index]) && query.length > 0)
+				searchDropdown.insertAdjacentHTML('afterBegin', createSuggestHTML(data[index]))
 		}
 	}
 
-	suggestAll = null
+	searchDropdown = null
+	searchBar = null
 }
 
 const hideSuggest = parent => {
-	if (parent.classList.contains('_has-suggest'))
-		parent.classList.remove('_has-suggest')
+	let searchDropdown = parent.querySelector('.search__dropdown');
 
-	const onRemoveSuggest = _ => {
-		resetSuggest(parent)
-	}
+	while (searchDropdown.firstChild)
+		searchDropdown.firstChild.remove()
 
-	setTimeout(onRemoveSuggest, getDurationTimeout())
-
-	hideOverlay()
+	searchDropdown = null
 }
 
 const resetSelected = parent => {
-	let selectedSuggest = parent.querySelector('.search__suggest._selected');
+	let selectedSuggest = parent.querySelector('._selected');
 
 	if (selectedSuggest)
 		selectedSuggest.classList.remove('_selected');
 
 	selectedSuggest = null;
 }
-
-
-const resetSuggest = parent => {
-	let suggestAll = parent.querySelectorAll('.search__suggest');
-
-	if (suggestAll.length > 0) {
-		for (let index = 0, length = suggestAll.length; index < length; index++) {
-			let suggest = suggestAll[index];
-			suggest.textContent = null
-
-			if (suggest.classList.contains('_visible'))
-				suggest.classList.remove('_visible');
-
-			suggest = null
-		}
-	}
-
-	suggestAll = null
-
-	resetSelected(parent)
-}
-
 
 const insertSelectedSuggest = (parent, suggest) => {
 	let searchBar = parent.querySelector('.search__bar');
@@ -75,80 +46,91 @@ const insertSelectedSuggest = (parent, suggest) => {
 }
 
 const chooseSuggest = (parent, last, direction) => {
-	let visibleSuggestAll = parent.querySelectorAll('.search__suggest._visible');
+	let suggestAll = parent.querySelectorAll('.search__suggest');
 
-	if (visibleSuggestAll.length > 0) {
+	if (suggestAll.length > 0) {
 		if (last !== null) {
 			if (direction === 40) {
-				if (visibleSuggestAll[last + 1]) {
-					visibleSuggestAll[last + 1].classList.add('_selected');
+				if (suggestAll[last + 1]) {
+					suggestAll[last + 1].classList.add('_selected');
 					lastSelected = last + 1
 				} else {
-					visibleSuggestAll[0].classList.add('_selected');
+					suggestAll[0].classList.add('_selected');
 					lastSelected = 0
 				}
 			} else if (direction === 38) {
-				if (visibleSuggestAll[last - 1]) {
-					visibleSuggestAll[last - 1].classList.add('_selected');
+				if (suggestAll[last - 1]) {
+					suggestAll[last - 1].classList.add('_selected');
 					lastSelected = last - 1
 				} else {
-					visibleSuggestAll[visibleSuggestAll.length - 1].classList.add('_selected');
-					lastSelected = visibleSuggestAll.length - 1
+					suggestAll[suggestAll.length - 1].classList.add('_selected');
+					lastSelected = suggestAll.length - 1
 				}
 			}
 		} else {
-			visibleSuggestAll[0].classList.add('_selected');
+			suggestAll[0].classList.add('_selected');
 			lastSelected = 0
 		}
 
-		let selectedSuggest = parent.querySelector('.search__suggest._selected');
+		let selectedSuggest = parent.querySelector('._selected');
 
 		if (selectedSuggest)
 			insertSelectedSuggest(parent, selectedSuggest.textContent)
 
 		selectedSuggest = null
 	}
-	visibleSuggestAll = null
+	suggestAll = null
 }
 
 const initSuggests = parent => {
-
 	let searchBar = parent.querySelector('.search__bar');
 
 	if (searchBar && !storage.settings.disableSearchSuggestions) {
 
 		const handleInpt = async _ => {
+			showOverlay()
+
 			lastSelected = null
 			let query = searchBar.value.trim();
 
-			if (query.length !== 0) {
+			if (query.length > 0) {
 				try {
 					let data = storage.settings.enableProxy
 						? await API.scrapeSuggestsProxy(query, getProxyOptions())
 						: await API.scrapeSuggests(query)
 
-					resetSuggest(parent)
-					showSuggest(parent, data)
+					resetSelected(parent)
+					hideSuggest(parent)
 
-					let visibleSuggestAll = parent.querySelectorAll('.search__suggest._visible');
+					if (data.length > 0)
+						addSuggest(parent, data)
 
-					for (let index = 0, length = visibleSuggestAll.length; index < length; index++) {
-						const visibleSuggest = visibleSuggestAll[index];
-						visibleSuggest.addEventListener('click', _ => {
-							insertSelectedSuggest(parent, visibleSuggest.textContent)
+					let suggestAll = parent.querySelectorAll('.search__suggest');
+
+					for (let index = 0, length = suggestAll.length; index < length; index++) {
+						const suggest = suggestAll[index];
+
+						suggest.addEventListener('click', _ => {
+							insertSelectedSuggest(parent, suggest.textContent)
 							searchBar.focus()
 						});
 					}
+
 				} catch (error) {
 					showToast('error', error.message);
 				} finally {
 					query = null
 				}
-
-			} else hideSuggest(parent)
+			} else {
+				hideSuggest(parent)
+				hideOverlay()
+			}
 		}
 
-		const handleBlur = _ => { hideSuggest(parent) }
+		const handleBlur = _ => {
+			hideSuggest(parent)
+			hideOverlay()
+		}
 
 		searchBar.addEventListener('input', handleInpt);
 		searchBar.addEventListener('blur', handleBlur)
