e22416c7ae3cbc7d765f152b2de72c45964214b1
pink-eye <github@virginson.anonaddy.com>
2021-11-22 14:33:00 +0630

    forgot to add with this commit 1b01f1f

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 const { series, parallel } = require('gulp')
 const { cleanDefault, transformHTML, minifyHTML, moveResources, moveImages, watchFiles } = require('./gulp/default')
 const { cleanStyles, compileStyles, watchStyles } = require('./gulp/styles')
-const { cleanScripts, launchTasksScripts, watchScripts } = require('./gulp/scripts')
+const { cleanScripts, concatScripts, watchScripts } = require('./gulp/scripts')
 const { clean, minifyStyles, uglifyScripts, compressImages } = require('./gulp/build')
 
 exports.default = series(cleanDefault, parallel(transformHTML, moveResources, moveImages), watchFiles)
 
 exports.styles = series(cleanStyles, compileStyles, watchStyles)
 
-exports.scripts = series(cleanScripts, launchTasksScripts, watchScripts)
+exports.scripts = series(cleanScripts, concatScripts, watchScripts)
 
 exports.build = series(
 	clean,
