b751b5b5429605c6b7429def9ae4444201a069ef
pink-eye <github@virginson.anonaddy.com>
2021-11-27 20:09:28 +0630

    improve code readability and logic of win-subscription

--- src/layouts/win-subscriptions/index.js
+++ src/layouts/win-subscriptions/index.js
@@ -3,29 +3,64 @@ import { AppStorage } from 'Global/app-storage'
 import { initPages, disablePages } from 'Components/grid-btns'
 import { fillAuthorCard } from 'Components/card/card-author'
 import { showToast } from 'Components/toast'
-import { resetIndicator } from 'Components/indicator'
+import { startIndicator, resetIndicator } from 'Components/indicator'
 
-const appStorage = new AppStorage()
-let storage = appStorage.getStorage()
+const updateSubscriptionData = async storage => {
+	let { subscriptions } = storage
 
-let isUpdated = false
+	if (subscriptions.length === 0) return undefined
 
-export const openWinSubs = async _ => {
-	const promises = []
+	let promises = []
 	let channelInfoArray = null
-	let subscriptions = getSelector('.subscriptions')
-	let authorCardAll = subscriptions.querySelectorAll('.author')
 	let btnSubscriptions = document.querySelector('button[data-win="subscriptions"]')
 
-	let { subscriptions: subs } = storage
+	for (let index = 0, { length } = subscriptions; index < length; index += 1) {
+		const subscription = subscriptions[index]
 
-	subs.length > authorCardAll.length
-		? initPages(subscriptions, subs, authorCardAll, 'author')
+		if (!btnSubscriptions.classList.contains('_active')) return
+
+		promises.push(API.scrapeChannelInfo(subscription.channelId))
+	}
+
+	try {
+		startIndicator()
+		channelInfoArray = [].concat.apply([], await Promise.all(promises))
+
+		if (channelInfoArray && channelInfoArray.length > 0) {
+			for (let index = 0, { length } = subscriptions; index < length; index += 1) {
+				const channelInfo = channelInfoArray[index]
+				const { authorThumbnails, author } = channelInfo
+
+				if (!btnSubscriptions.classList.contains('_active')) return
+
+				subscriptions[index].avatar = authorThumbnails.at(-1).url
+				subscriptions[index].name = author
+			}
+		}
+	} catch ({ message }) {
+		showToast('error', message)
+	} finally {
+		resetIndicator()
+	}
+
+	btnSubscriptions = null
+
+	return subscriptions
+}
+
+export const openWinSubs = async _ => {
+	const appStorage = new AppStorage()
+	let storage = appStorage.getStorage()
+	let subscriptions = getSelector('.subscriptions')
+	let authorCardAll = subscriptions.querySelectorAll('.author')
+
+	storage.subscriptions.length > authorCardAll.length
+		? initPages(subscriptions, storage.subscriptions, authorCardAll, 'author')
 		: disablePages(subscriptions)
 
 	for (let index = 0, { length } = authorCardAll; index < length; index += 1) {
 		const authorCard = authorCardAll[index]
-		const sub = subs[index]
+		const sub = storage.subscriptions[index]
 
 		if (sub) {
 			const { name, channelId, avatar = '' } = sub
@@ -43,41 +78,14 @@ export const openWinSubs = async _ => {
 		} else authorCard.hidden = true
 	}
 
-	if (!isUpdated) {
-		for (let index = 0, { length } = subs; index < length; index += 1) {
-			const subscription = subs[index]
+	const updatedSubscriptions = await updateSubscriptionData(storage)
 
-			if (!btnSubscriptions.classList.contains('_active')) return
+	if (updatedSubscriptions) {
+		storage.subscriptions = updatedSubscriptions
 
-			promises.push(API.scrapeChannelInfo(subscription.channelId))
-		}
-
-		try {
-			channelInfoArray = [].concat.apply([], await Promise.all(promises))
-		} catch ({ message }) {
-			showToast('error', message)
-		} finally {
-			resetIndicator()
-		}
-
-		if (channelInfoArray && channelInfoArray.length > 0) {
-			for (let index = 0, { length } = subs; index < length; index += 1) {
-				const sub = storage.subscriptions[index]
-				const channelInfo = channelInfoArray[index]
-
-				if (!btnSubscriptions.classList.contains('_active')) return
-
-				sub.avatar = channelInfo.authorThumbnails.at(-1).url
-				sub.name = channelInfo.author
-			}
-
-			appStorage.updateStorage(storage)
-
-			isUpdated = true
-		}
-
-		subscriptions = null
-		authorCardAll = null
-		btnSubscriptions = null
+		appStorage.updateStorage(storage)
 	}
+
+	subscriptions = null
+	authorCardAll = null
 }
