124e8315f0b19edaa207f5c396be434a8f4974e3
pink-eye <3fvvu3fx@anonaddy.me>
2022-02-23 01:08:52 +0630

    UI: simplify and reduce skeleton implementation

--- src/renderer/components/skeleton/index.js
+++ src/renderer/components/skeleton/index.js
@@ -1,30 +1,19 @@
-import { getDurationTimeout } from 'Global/utils'
-
 const removeSkeleton = skeleton => {
 	queueMicrotask(() => {
 		let givenSkeleton = skeleton
-		const timeout = getDurationTimeout()
 
-		if (!givenSkeleton.classList.contains('_active')) givenSkeleton.classList.add('_removing')
+		givenSkeleton.hidden ||= true
 
-		const hideSkeleton = () => {
-			givenSkeleton.hidden ||= true
-			givenSkeleton = null
-		}
-
-		timeout > 0
-			? givenSkeleton.addEventListener('transitionend', hideSkeleton, { once: true })
-			: hideSkeleton()
+		givenSkeleton = null
 	})
 }
 
 const resetSkeleton = skeleton => {
 	queueMicrotask(() => {
 		let givenSkeleton = skeleton
-		if (givenSkeleton.classList.contains('_removing'))
-			givenSkeleton.classList.remove('_removing')
 
 		givenSkeleton.hidden &&= false
+
 		givenSkeleton = null
 	})
 }
