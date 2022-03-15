8b834c294bd44b648069fbcb84d19d5f7f4865c5
pink-eye <3fvvu3fx@anonaddy.me>
2022-03-15 18:40:37 +0630

    refactor: rewrite update checking logic

--- src/renderer/global/js/checkForUpdate.js
+++ src/renderer/global/js/checkForUpdate.js
@@ -1,12 +1,17 @@
-const checkForUpdate = async () => {
-	const RELEASE_URL = 'https://api.github.com/repos/pink-eye/vvvideo/releases/latest'
+const checkForUpdate = () => {
+	return new Promise((resolve, reject) => {
+		const RELEASE_URL = 'https://api.github.com/repos/pink-eye/vvvideo/releases/latest'
 
-	const dataJson = await fetch(RELEASE_URL)
-	const data = await dataJson.json()
+		fetch(RELEASE_URL)
+			.then(res => res.json())
+			.then(async data => {
+				const LATEST_VERSION = data.tag_name
+				const CURRENT_VERSION = await API.getAppVersion()
 
-	const appVersion = await API.getAppVersion()
-
-	return [data.tag_name, `v${appVersion}`, data]
+				resolve([LATEST_VERSION, `v${CURRENT_VERSION}`, data])
+			})
+			.catch(() => reject('Error was occurred after update checking...'))
+	})
 }
 
 export default checkForUpdate
