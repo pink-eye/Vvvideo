03c9f670f7d49032d76e99ae1df2844c29f5a3d1
pink-eye <3fvvu3fx@anonaddy.me>
2022-03-17 15:43:28 +0630

    refactor: make the dialog-sb a class

--- src/renderer/components/dialog-sb/index.js
+++ src/renderer/components/dialog-sb/index.js
@@ -1,224 +1,153 @@
 import cs from 'Global/cacheSelectors'
-import {
-	convertDurationToSeconds,
-	convertSecondsToDuration,
-	isChild,
-} from 'Global/utils'
+import { convertDurationToSeconds, convertSecondsToDuration, isChild } from 'Global/utils'
 import AppStorage from 'Global/AppStorage'
 import showToast from 'Components/toast'
+import MicroModal from 'micromodal'
 import { formatDuration } from 'Components/dialog-sb/helper'
 import { uuidv4 } from 'Components/video-controls/sponsorblock'
 
-let isRecording = false
-let modal = null
+export default class DialogSB {
+	constructor() {
+		this.dialogSb = cs.get('.dialog-sb')
+		this.dialogSbStart = this.dialogSb.querySelector('input#start')
+		this.dialogSbEnd = this.dialogSb.querySelector('input#end')
+		this.dialogSbWarning = this.dialogSb.querySelector('.dialog-sb__warning')
 
-const hideInvalidUI = () => {
-	let dialogSb = cs.get('.dialog-sb')
-	let dialogSbStart = dialogSb.querySelector('input#start')
-	let dialogSbEnd = dialogSb.querySelector('input#end')
-	let dialogSbWarning = dialogSb.querySelector('.dialog-sb__warning')
-
-	if (dialogSbStart.classList.contains('_error')) {
-		dialogSbStart.classList.remove('_error')
-		dialogSbEnd.classList.remove('_error')
-		dialogSbWarning.hidden = true
+		this.handleInputDialog = this.handleInputDialog.bind(this)
+		this.handleClickDialog = this.handleClickDialog.bind(this)
 	}
 
-	dialogSb = null
-	dialogSbStart = null
-	dialogSbEnd = null
-	dialogSbWarning = null
-}
-
-const handleInputField = ({ target }) => {
-	let dialogSbField = target
-
-	dialogSbField.value = formatDuration(dialogSbField.value)
-
-	hideInvalidUI()
-
-	dialogSbField = null
-}
-
-const handleInputDialog = event => {
-	let { target } = event
-
-	if (target.matches('input[type="text"]')) {
-		handleInputField(event)
+	#hideInvalidUI() {
+		if (this.dialogSbStart.classList.contains('_error')) {
+			this.dialogSbStart.classList.remove('_error')
+			this.dialogSbEnd.classList.remove('_error')
+			this.dialogSbWarning.hidden = true
+		}
 	}
 
-	target = null
-}
-
-const handleClickDialog = event => {
-	let { target } = event
-
-	if (isChild(target, '.dialog-sb__btn_send')) {
-		sendSegmentSB()
+	#showInvalidUI() {
+		if (!this.dialogSbStart.classList.contains('_error')) {
+			this.dialogSbStart.classList.add('_error')
+			this.dialogSbEnd.classList.add('_error')
+			this.dialogSbWarning.hidden = false
+			this.dialogSbStart.focus()
+		}
 	}
 
-	if (isChild(target, '.dialog-sb__btn_cancel')) {
-		modal.close()
+	handleInputField({ target }) {
+		let dialogSbField = target
+
+		dialogSbField.value = formatDuration(dialogSbField.value)
+
+		this.#hideInvalidUI()
+
+		dialogSbField = null
 	}
 
-	target = null
-}
+	handleInputDialog(event) {
+		if (event.target.matches('input[type="text"]')) {
+			this.handleInputField(event)
+		}
+	}
 
-const showDialogSB = () => {
-	if (document.fullscreenElement) document.exitFullscreen()
+	handleClickDialog(event) {
+		if (isChild(event.target, '.dialog-sb__btn_send')) {
+			this.send()
+		}
 
-	let video = cs.get('video')
-	let audio = cs.get('.video').querySelector('audio')
+		if (isChild(event.target, '.dialog-sb__btn_cancel')) {
+			MicroModal.close('dialog-sb')
+		}
+	}
 
-	video.pause()
-	audio && audio.pause()
+	show() {
+		if ('beforeOpen' in this.config) {
+			this.config.beforeOpen()
+		}
 
-	hideInvalidUI()
+		this.dialogSb.addEventListener('click', this.handleClickDialog)
+		this.dialogSb.addEventListener('input', this.handleInputDialog)
 
-	let dialogSb = cs.get('.dialog-sb')
+		MicroModal.show('dialog-sb', { awaitCloseAnimation: true, onClose: () => this.reset() })
+	}
 
-	dialogSb.addEventListener('click', handleClickDialog)
-	dialogSb.addEventListener('input', handleInputDialog)
+	isValidFields() {
+		const { value: valueStart } = this.dialogSbStart
+		const { value: valueEnd } = this.dialogSbEnd
 
-	modal.open('dialog-sb')
+		let patternTimecodeMMSS = /\b(?<!>)([0-5]?[0-9]):([0-5][0-9])(?!<)\b/gm
+		let patternTimecodeHHMMSS = /\b(2[0-3]|[0-1]?[\d]):[0-5][\d]:[0-5][\d]\b/gm
 
-	video = null
-	audio = null
-	dialogSb = null
-}
+		const isValid =
+			((valueStart.match(patternTimecodeHHMMSS) && valueEnd.match(patternTimecodeHHMMSS)) ||
+				(valueStart.match(patternTimecodeMMSS) && valueEnd.match(patternTimecodeMMSS))) &&
+			convertDurationToSeconds(valueStart) < convertDurationToSeconds(valueEnd) &&
+			convertDurationToSeconds(valueEnd) <= this.config.duration
 
-const recordSegmentSB = ({ onStart, onEnd }) => {
-	const appStorage = new AppStorage()
-	const storage = appStorage.get()
-	const { disableSponsorblock } = storage.settings
+		return isValid
+	}
 
-	if (disableSponsorblock) return
+	send() {
+		if (this.isValidFields()) {
+			const { value: valueStart } = this.dialogSbStart
+			const { value: valueEnd } = this.dialogSbEnd
+			const { id: category } = this.dialogSb.querySelector('input[name="category"]:checked')
 
-	modal = new GraphModal({ isClose: handleCloseModal })
+			const startTime = convertDurationToSeconds(valueStart)
+			const endTime = convertDurationToSeconds(valueEnd)
+			const segment = { startTime, endTime, category }
 
-	let video = cs.get('video')
+			API.postSponsorblockInfo(this.config.videoId, uuidv4(), segment)
+				.then(() => {
+					MicroModal.close('dialog-sb')
+					showToast('good', 'Segment was sent successfully!')
+				})
+				.catch(({ message }) => showToast('error', message))
+		} else this.#showInvalidUI()
+	}
 
-	if (!video.paused || !video.ended) {
-		let dialogSb = cs.get('.dialog-sb')
+	toggleRecording(params) {
+		const appStorage = new AppStorage()
+		const storage = appStorage.get()
+		const { disableSponsorblock } = storage.settings
 
-		if (!isRecording) {
-			let dialogSbStart = dialogSb.querySelector('input#start')
-			dialogSbStart.value = convertSecondsToDuration(video.currentTime)
+		if (disableSponsorblock) return
 
-			onStart()
+		if (!this.isRecording) {
+			this.dialogSbStart.value = convertSecondsToDuration(params.currentTime)
+
 			showToast('info', 'Recording of segment is started...')
 
-			dialogSbStart = null
+			if ('onStartRecording' in params) {
+				params.onStartRecording()
+			}
 		} else {
-			let dialogSbEnd = dialogSb.querySelector('input#end')
-			dialogSbEnd.value = convertSecondsToDuration(video.currentTime)
+			this.dialogSbEnd.value = convertSecondsToDuration(params.currentTime)
 
-			onEnd()
-			showDialogSB()
+			this.show()
 
-			dialogSbEnd = null
+			if ('onEndRecording' in params) {
+				params.onEndRecording()
+			}
 		}
 
-		isRecording = !isRecording
-		dialogSb = null
-	} else showToast('error', 'You must play video!')
-
-	video = null
-}
-
-const resetDialogSB = () => {
-	let dialogSb = cs.get('.dialog-sb')
-	let dialogSbStart = dialogSb.querySelector('input#start')
-	let dialogSbEnd = dialogSb.querySelector('input#end')
-
-	hideInvalidUI()
-
-	dialogSbStart.value = ''
-	dialogSbEnd.value = ''
-
-	dialogSb.removeEventListener('click', handleClickDialog)
-	dialogSb.removeEventListener('input', handleInputDialog)
-
-	isRecording &&= false
-
-	dialogSb = null
-	dialogSbStart = null
-	dialogSbEnd = null
-}
-
-const isValidFields = () => {
-	let dialogSb = cs.get('.dialog-sb')
-
-	const { value: valueStart } = dialogSb.querySelector('input#start')
-	const { value: valueEnd } = dialogSb.querySelector('input#end')
-	const { duration } = cs.get('video')
-
-	let patternTimecodeMMSS = /\b(?<!>)([0-5]?[0-9]):([0-5][0-9])(?!<)\b/gm
-	let patternTimecodeHHMMSS = /\b(2[0-3]|[0-1]?[\d]):[0-5][\d]:[0-5][\d]\b/gm
-
-	const result =
-		((valueStart.match(patternTimecodeHHMMSS) && valueEnd.match(patternTimecodeHHMMSS)) ||
-			(valueStart.match(patternTimecodeMMSS) && valueEnd.match(patternTimecodeMMSS))) &&
-		convertDurationToSeconds(valueStart) < convertDurationToSeconds(valueEnd) &&
-		convertDurationToSeconds(valueEnd) <= duration
-
-	dialogSb = null
-
-	return result
-}
-
-const showInvalidUI = () => {
-	let dialogSb = cs.get('.dialog-sb')
-	let dialogSbStart = dialogSb.querySelector('input#start')
-	let dialogSbEnd = dialogSb.querySelector('input#end')
-	let dialogSbWarning = dialogSb.querySelector('.dialog-sb__warning')
-
-	if (!dialogSbStart.classList.contains('_error')) {
-		dialogSbStart.classList.add('_error')
-		dialogSbEnd.classList.add('_error')
-		dialogSbWarning.hidden ||= false
-		dialogSbStart.focus()
+		this.isRecording = !this.isRecording
 	}
 
-	dialogSb = null
-	dialogSbStart = null
-	dialogSbEnd = null
-	dialogSbWarning = null
+	init(config) {
+		this.config = config
+		this.isRecording = false
+	}
+
+	reset() {
+		this.#hideInvalidUI()
+
+		this.dialogSbStart.value = ''
+		this.dialogSbEnd.value = ''
+
+		this.dialogSb.removeEventListener('click', this.handleClickDialog)
+		this.dialogSb.removeEventListener('input', this.handleInputDialog)
+
+		this.isRecording &&= false
+	}
 }
-
-const sendSegmentSB = async () => {
-	if (isValidFields()) {
-		let dialogSb = cs.get('.dialog-sb')
-
-		const { id: videoId } = cs.get('.video').dataset
-		const { id: category } = dialogSb.querySelector('input[name="category"]:checked')
-		const { value: valueStart } = dialogSb.querySelector('input#start')
-		const { value: valueEnd } = dialogSb.querySelector('input#end')
-
-		const startTime = convertDurationToSeconds(valueStart)
-		const endTime = convertDurationToSeconds(valueEnd)
-		const segment = { startTime, endTime, category }
-
-		try {
-			await API.postSponsorblockInfo(videoId, uuidv4(), segment)
-			modal.close()
-			showToast('good', 'Segment was sent successfully!')
-		} catch ({ message }) {
-			showToast('error', message)
-		}
-
-		dialogSb = null
-	} else showInvalidUI()
-}
-
-const handleCloseModal = () => {
-	let video = cs.get('video')
-
-	video.play()
-
-	resetDialogSB()
-
-	video = null
-}
-
-export default recordSegmentSB
