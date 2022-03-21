8f68531f6964eb0d90eb6d1118c0938d43aa201e
pink-eye <3fvvu3fx@anonaddy.me>
2022-03-21 20:40:12 +0630

    chore: remove unnecessary code

--- src/renderer/components/spoiler/index.js
+++ src/renderer/components/spoiler/index.js
@@ -41,38 +41,3 @@ const Spoiler = () => {
 }
 
 export default Spoiler
-
-// const toggleSpoiler = ({ currentTarget }) => {
-// 	let spoiler = currentTarget.closest('.spoiler')
-
-// 	if (spoiler.classList.contains('_opened')) {
-// 		spoiler.removeAttribute('style')
-// 		spoiler.classList.remove('_opened')
-// 	} else {
-// 		const { offsetHeight } = spoiler.querySelector('.spoiler__content')
-
-// 		spoiler.style.setProperty('--height-content', `${offsetHeight}px`)
-// 		spoiler.classList.add('_opened')
-// 	}
-
-// 	spoiler = null
-// }
-
-// export const initSpoiler = spoiler => {
-// 	let spoilerHead = spoiler.querySelector('.spoiler__head')
-
-// 	spoilerHead.addEventListener('click', toggleSpoiler)
-
-// 	spoilerHead = null
-// }
-
-// export const destroySpoiler = spoiler => {
-// 	let spoilerHead = spoiler.querySelector('.spoiler__head')
-
-// 	spoiler.removeAttribute('style')
-// 	spoiler.classList.remove('_opened')
-
-// 	spoilerHead.removeEventListener('click', toggleSpoiler)
-
-// 	spoilerHead = null
-// }
