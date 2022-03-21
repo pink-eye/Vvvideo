a06b9209c2bd68b3cd7be6665fe4cfa5bc7a542a
pink-eye <3fvvu3fx@anonaddy.me>
2022-03-21 20:45:51 +0630

    refactor: rewrite playlist using closure way

--- src/renderer/layouts/win-playlist/index.js
+++ src/renderer/layouts/win-playlist/index.js
@@ -4,193 +4,162 @@ import AppStorage from 'Global/AppStorage'
 import { resetGrid } from 'Components/grid'
 import showToast from 'Components/toast'
 import { fillVideoCard } from 'Components/card/card-video'
-import { initPages, disablePages } from 'Components/grid-btns'
+import Pages from 'Components/grid-btns'
 import { resetSkeleton, removeSkeleton } from 'Components/skeleton'
 import { fillAuthorCard, resetAuthorCard } from 'Components/card/card-author'
 
-const getPlaylistData = id => {
-	const appStorage = new AppStorage()
-	const { proxy, enableProxy } = appStorage.get().settings
+const WinPlaylist = () => {
+	const playlist = cs.get('.playlist')
+	const pages = Pages()
 
-	return enableProxy ? API.scrapePlaylistVideosProxy(id, proxy) : API.scrapePlaylistVideos(id)
-}
+	const fetchData = id => {
+		const appStorage = new AppStorage()
+		const { proxy, enableProxy } = appStorage.get().settings
 
-const openWinPlaylist = data => {
-	let playlist = cs.get('.playlist')
-	let playlistName = playlist.querySelector('.playlist__name span')
-	let playlistViews = playlist.querySelector('.playlist__views')
-	let playlistVideoCount = playlist.querySelector('.playlist__video-count')
-	let playlistLastUpdated = playlist.querySelector('.playlist__last-upd')
-	let playlistDuration = playlist.querySelector('.playlist__duration')
-	let titleSkeleton = playlist.querySelector('.title-skeleton')
-	let partSkeletonAll = playlist.querySelectorAll('.part-skeleton')
-	let authorCard = playlist.querySelector('.author')
-
-	const { title, estimatedItemCount, views, lastUpdated, items, author, continuation, id } = data
-
-	if (playlistName.textContent !== title) playlistName.textContent = title
-
-	removeSkeleton(titleSkeleton)
-
-	playlistVideoCount.textContent = `${items.length} / ${estimatedItemCount} available videos`
-	playlistViews.textContent = normalizeCount(views)
-
-	playlistLastUpdated.textContent = lastUpdated
-
-	let duration = 0
-
-	for (let index = 0, { length } = items; index < length; index += 1) {
-		const video = items[index]
-		duration += video.durationSec
+		return enableProxy ? API.scrapePlaylistVideosProxy(id, proxy) : API.scrapePlaylistVideos(id)
 	}
 
-	playlistDuration.textContent = convertSecondsToDuration(duration)
+	const fill = data => {
+		const playlistName = playlist.querySelector('.playlist__name span')
+		const playlistViews = playlist.querySelector('.playlist__views')
+		const playlistVideoCount = playlist.querySelector('.playlist__video-count')
+		const playlistLastUpdated = playlist.querySelector('.playlist__last-upd')
+		const playlistDuration = playlist.querySelector('.playlist__duration')
+		const titleSkeleton = playlist.querySelector('.title-skeleton')
+		const partSkeletonAll = playlist.querySelectorAll('.part-skeleton')
+		const authorCard = playlist.querySelector('.author')
 
-	if (partSkeletonAll.length > 0) {
-		for (let index = 0, { length } = partSkeletonAll; index < length; index += 1) {
-			let partSkeleton = partSkeletonAll[index]
+		const { title, estimatedItemCount, views, lastUpdated, items, author, continuation, id } =
+			data
 
-			removeSkeleton(partSkeleton)
+		if (playlistName.textContent !== title) playlistName.textContent = title
 
-			partSkeleton = null
-		}
-	}
-
-	let authorParams = {
-		parent: authorCard,
-		name: author.name,
-		avatarSrc: author.bestAvatar.url,
-		id: author.channelID,
-	}
-
-	fillAuthorCard(authorParams)
-
-	authorParams = null
-
-	let videoAll = playlist.querySelectorAll('.card')
-
-	items.length > videoAll.length
-		? initPages(playlist, items, videoAll, 'video', continuation)
-		: disablePages(playlist)
-
-	for (let index = 0, { length } = videoAll; index < length; index += 1) {
-		let video = videoAll[index]
-
-		video.classList.add('_playlist-video')
-		video.dataset.playlistId = id
-
-		items[index] ? fillVideoCard(video, index, items) : (video.hidden = true)
-
-		video = null
-	}
-
-	videoAll = null
-	playlist = null
-	playlistViews = null
-	playlistVideoCount = null
-	playlistDuration = null
-	partSkeletonAll = null
-	playlistName = null
-	playlistLastUpdated = null
-	titleSkeleton = null
-	authorCard = null
-}
-
-export const resetWinPlaylist = () => {
-	let playlist = cs.get('.playlist')
-	let playlistName = playlist.querySelector('.playlist__name span')
-	let playlistViews = playlist.querySelector('.playlist__views')
-	let playlistVideoCount = playlist.querySelector('.playlist__video-count')
-	let playlistLastUpdated = playlist.querySelector('.playlist__last-upd')
-	let playlistDuration = playlist.querySelector('.playlist__duration')
-	let titleSkeleton = playlist.querySelector('.title-skeleton')
-	let partSkeletonAll = playlist.querySelectorAll('.part-skeleton')
-	let authorCard = playlist.querySelector('.author')
-
-	resetSkeleton(titleSkeleton)
-	playlistName.textContent = '...'
-
-	resetAuthorCard(authorCard)
-
-	if (partSkeletonAll.length > 0) {
-		for (let index = 0, { length } = partSkeletonAll; index < length; index += 1) {
-			let partSkeleton = partSkeletonAll[index]
-
-			resetSkeleton(partSkeleton)
-
-			partSkeleton = null
-		}
-	}
-
-	playlistViews.textContent = ''
-	playlistVideoCount.textContent = ''
-	playlistDuration.textContent = ''
-	playlistLastUpdated.textContent = ''
-
-	resetGrid(playlist)
-
-	playlist = null
-	playlistViews = null
-	playlistVideoCount = null
-	authorCard = null
-	playlistLastUpdated = null
-	playlistDuration = null
-	playlistName = null
-	titleSkeleton = null
-	partSkeletonAll = null
-}
-
-const fillSomeInfoPlaylist = ({ title = '', author = '', id = '' }) => {
-	let playlist = cs.get('.playlist')
-	let playlistName = playlist.querySelector('.playlist__name span')
-	let titleSkeleton = playlist.querySelector('.title-skeleton')
-	let authorCard = playlist.querySelector('.author')
-
-	if (!isEmpty(title) && title !== '...') {
-		playlistName.textContent = title
 		removeSkeleton(titleSkeleton)
-	}
 
-	let authorParams = {
-		parent: authorCard,
-		name: author,
-		id,
-	}
+		playlistVideoCount.textContent = `${items.length} / ${estimatedItemCount} available videos`
+		playlistViews.textContent = normalizeCount(views)
 
-	fillAuthorCard(authorParams)
+		playlistLastUpdated.textContent = lastUpdated
 
-	playlist = null
-	playlistName = null
-	authorCard = null
-	titleSkeleton = null
-	authorParams = null
-}
+		let duration = 0
 
-export const prepareWinPlaylist = async (btnWin, id) => {
-	let params = {}
-
-	if (btnWin?.classList.contains('card')) {
-		params = {
-			title: btnWin.querySelector('.card__title span').textContent,
-			author: btnWin.querySelector('.card__channel').dataset.name,
-			id: btnWin.querySelector('.card__channel').dataset.id,
+		for (let index = 0, { length } = items; index < length; index += 1) {
+			const video = items[index]
+			duration += video.durationSec
 		}
 
-		fillSomeInfoPlaylist(params)
+		playlistDuration.textContent = convertSecondsToDuration(duration)
+
+		if (partSkeletonAll.length) {
+			for (let index = 0, { length } = partSkeletonAll; index < length; index += 1) {
+				const partSkeleton = partSkeletonAll[index]
+
+				removeSkeleton(partSkeleton)
+			}
+		}
+
+		const authorParams = {
+			parent: authorCard,
+			name: author.name,
+			avatarSrc: author.bestAvatar.url,
+			id: author.channelID,
+		}
+
+		fillAuthorCard(authorParams)
+
+		const videoAll = playlist.querySelectorAll('.card')
+
+		if (items.length > videoAll.length) {
+			pages.init({ element: playlist, data: items, type: 'video', continuation })
+		}
+
+		for (let index = 0, { length } = videoAll; index < length; index += 1) {
+			const video = videoAll[index]
+
+			if (items[index]) {
+				video.classList.add('_playlist-video')
+				video.dataset.playlistId = id
+				fillVideoCard(video, index, items)
+			} else {
+				video.hidden = true
+			}
+		}
 	}
 
-	let data = null
+	const fillPart = ({ title = '', author = '', id = '' }) => {
+		const playlistName = playlist.querySelector('.playlist__name span')
+		const titleSkeleton = playlist.querySelector('.title-skeleton')
+		const authorCard = playlist.querySelector('.author')
 
-	try {
-		data = await getPlaylistData(id)
-	} catch ({ message }) {
-		showToast('error', message)
-		return
+		if (!isEmpty(title) && title !== '...') {
+			playlistName.textContent = title
+			removeSkeleton(titleSkeleton)
+		}
+
+		const authorParams = {
+			parent: authorCard,
+			name: author,
+			id,
+		}
+
+		fillAuthorCard(authorParams)
 	}
 
-	let playlist = cs.get('.playlist')
+	const init = ({ btnWin, id }) => {
+		if (btnWin?.classList.contains('card')) {
+			const partialData = {
+				title: btnWin.querySelector('.card__title span').textContent,
+				author: btnWin.querySelector('.card__channel').dataset.name,
+				id: btnWin.querySelector('.card__channel').dataset.id,
+			}
 
-	if (playlist.classList.contains('_active')) openWinPlaylist(data)
+			fillPart(partialData)
+		}
 
-	playlist = null
+		fetchData(id)
+			.then(data => {
+				if (playlist.classList.contains('_active')) fill(data)
+			})
+			.catch(({ message }) => showToast('error', message))
+	}
+
+	const reset = () => {
+		const playlistName = playlist.querySelector('.playlist__name span')
+		const playlistViews = playlist.querySelector('.playlist__views')
+		const playlistVideoCount = playlist.querySelector('.playlist__video-count')
+		const playlistLastUpdated = playlist.querySelector('.playlist__last-upd')
+		const playlistDuration = playlist.querySelector('.playlist__duration')
+		const titleSkeleton = playlist.querySelector('.title-skeleton')
+		const partSkeletonAll = playlist.querySelectorAll('.part-skeleton')
+		const authorCard = playlist.querySelector('.author')
+
+		resetSkeleton(titleSkeleton)
+		resetAuthorCard(authorCard)
+		playlistName.textContent = '...'
+
+		if (partSkeletonAll.length > 0) {
+			for (let index = 0, { length } = partSkeletonAll; index < length; index += 1) {
+				const partSkeleton = partSkeletonAll[index]
+
+				resetSkeleton(partSkeleton)
+			}
+		}
+
+		playlistViews.textContent = ''
+		playlistVideoCount.textContent = ''
+		playlistDuration.textContent = ''
+		playlistLastUpdated.textContent = ''
+
+		resetGrid(playlist)
+		pages.reset()
+	}
+
+	return {
+		init,
+		reset,
+	}
 }
+
+const winPlaylist = WinPlaylist()
+
+export default winPlaylist
